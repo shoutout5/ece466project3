467472ec26724b4529f5144d2b266f8686c589bc
Andrew S <asoutha@ncsu.edu>
2011-11-29 21:06:55 -0500

    DDG code

---
+++ defines.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   defines.h
+ * Author: andrew
+ *
+ * Created on November 29, 2011, 2:23 PM
+ */
+
+#ifndef DEFINES_H
+#define	DEFINES_H
+
+#define GO_LIVE 0
+#define DEAD 1
+
+#endif	/* DEFINES_H */
+
